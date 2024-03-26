2e82a44af449208ac30ca223f023c4a56abbc2f4
br0nke <woitekas@gmail.com>
2024-03-26 16:38:36 +0000

    chat fixes + forms func fix + front end tweaks

--- core/codes/forms.py
+++ core/codes/forms.py
@@ -1,16 +1,23 @@
 from django import forms
 from .models import CodeListing
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 
 class CodeListingForm(forms.ModelForm):
     class Meta:
         model = CodeListing
         fields = ('title', 'description', 'category', 'image', 'link')
 
-def validate_link(link):
-  if "github.com" not in link.lower():
-    raise ValueError("Link must be from 'github.com'!")
-  
-def get_full_url(link):
-    if not link.startswith('http'):
-        link = f'http://{link}'
-    return link
+    def clean_link(self):
+        link = self.cleaned_data['link']
+
+        validator = URLValidator()
+        try:
+            validator(link)
+        except ValidationError as e:
+            raise ValidationError("Invalid link format. Please enter a valid URL.")
+
+        if 'github.com' not in link.lower():
+            raise ValidationError("Link must be from 'github.com'!")
+
+        return link
